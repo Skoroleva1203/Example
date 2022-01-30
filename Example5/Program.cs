35a2f831a51015a16c7ad1cf1ae1040555586657
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-01-30 20:14:26 +0400

    Добавили Example5, Выборочное приветствие

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console . Write ( " Введите имя пользователя: " );
+string username = Console.ReadLine();
+
+if(username.ToLower() == "светлана")
+{
+    Console . WriteLine ( " Ура, это же Светлана! " );
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
