2bc00f788a184feec833c374df62a9e6dfd08ee9
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-02 20:57:39 +0400

    Нашли заданный элемент массива

---
+++ Example10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти элемент массива, равный find
+
+int[] array = {1,52,6,7,15,24,75,9,17};
+
+int n = array.Length;
+int find = 15;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+    index ++;    
+}
